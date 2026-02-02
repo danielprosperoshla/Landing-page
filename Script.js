869e3591cb0dfa0f08c42b26cf44fbad69525d15
danielprosperoshla <danielprosperoshla@gmail.com>
2026-02-02 21:16:42 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
+// Select button
+const joinBtn = document.getElementById("joinBtn");
 
+// Add click event
+joinBtn.addEventListener("click", function () {
+    alert("Welcome to NexaDev Academy! 🚀\nRegistration opens soon.");
+});
